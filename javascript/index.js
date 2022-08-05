b4800de7a793cbd98f6db8e8301c95b3e689807a
panosjapan7 <panosjapan@gmail.com>
2022-08-05 13:23:04 +0200

    comments to accordion functionality

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,42 @@
+/*--------------------------------- 
+    ACCORDION FUNCTIONALIY
+---------------------------------*/
+// Runs the code below *after* the page has loaded all its elements
 window.addEventListener("load", function() {
 
+    // Finds all HTML elements with class "question"
     var questions = document.getElementsByClassName("question");
-    console.log(questions)
-    console.log(questions[0])
-    var arrows = document.getElementsByClassName("arrow");
 
+    // Loops through the array which contains all HTML elements with class "question"
     for (let i = 0; i < questions.length; i++) {
+        
+        // Adds an event listener to every HTML element with class "question"
         questions[i].addEventListener("click", function(e) {
+            
+            // Boolean flag: If the the clicked Element contains "active" in its class, it sets the active to true
             let active = this.classList.contains("active");
-            let answer = this.nextElementSibling;
-            // let arrow = arr;
+            console.log("active", active);
 
+            // Saves the clicked element's next sibling ("answer element") to the 'answer' variable
+            let answer = this.nextElementSibling;
+
+            console.log(...questions);
+
+            // Loops through the array which contains all HTML elements with class "question"
             [...questions].forEach(function(item) {
+                
+                // Removes "active" from the class of every "question element"
                 item.classList.remove("active");
+                
+                // HIDES THE ANSWER OF "active" ELEMENT
+                //Sets the maxHeight of the next sibling of "active" question element to null
                 item.nextElementSibling.style.maxHeight = null;
+                
+                //Sets the maxHeight of the next sibling of "active" question element to 0px
                 item.nextElementSibling.style.marginBottom = "0px";
             });
 
+            // SHOWS THE ANSWER OF CLICKED "question" ELEMENT if it's not active
             if (!active) {
                 this.classList.add("active");
                 answer.style.maxHeight = "100px";
